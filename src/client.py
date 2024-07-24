26adf0a9f638bb3aa28766f0ec7ea81dd91bb5c5
NeiraJ <johnny.neira@gmail.com>
2024-07-24 13:36:32 +0200

    Refactorisation de la gestion du panier d'achat pour les clients
    - Intégration de la classe ShoppingCart dans le modèle Client pour gérer les achats.
    - Remplacement du tableau interne `self.basket` par une instance de ShoppingCart pour une gestion plus structurée des articles achetés.
    - Correction de la syntaxe et clarification des commentaires dans la méthode `facture` pour améliorer la lisibilité et la maintenabilité du code.
    - Ajout d'une note sur la nécessité de mettre à jour le stock lors de l'achat, indiquant que cette opération doit être effectuée mais n'est pas implémentée dans ce commit.
    
    Ces modifications visent à améliorer la structure et la clarté du code en utilisant une classe dédiée pour gérer les paniers d'achat, tout en soulignant l'importance de la gestion du stock dans le processus d'achat.

--- src/client.py
+++ src/client.py
@@ -1,19 +1,23 @@
+from src.shopping_cart import ShoppingCart
+
+
 class Client:
     """Ce qui permet à un client de faire ses achats"""
+
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
-        self.basket = []
+        self.basket = ShoppingCart(name, [])
 
     def achat(self, product, quantity, unit_price):
         """La quantité est exprimée en kilo (float ou à la pièce"""
         self.basket += (product, quantity, unit_price)
+
     def facture(self):
-        toll=0
+        toll = 0
         for achat in self.basket:
             (product, quantity, unit_price) = achat
             toll += (quantity * unit_price)
             # stock (product) -= quantity
             # Ce n'est pas l'endroit pour le faire,
             # mais ça doit être fait
-
