97d270ad87694f4e3c900f8f8d7403fcc55ceb51
ayl001 <76446556+ayl001@users.noreply.github.com>
2024-07-24 14:51:38 +0200

    Update bill.py

--- src/bill.py
+++ src/bill.py
@@ -1,14 +1,18 @@
 import client
-class Bill
-    dailyBill=[]
-    def __init__(self,client: client.Client):
-        amount= 0.0
-        print(f"Facture de {client.prenom} {client.nom}")
+
+
+class Bill:
+    dailyBill = []
+
+    def __init__(self, my_client: client.Client):
+        amount = 0.0
+        print(f"Facture de {my_client.surname} {my_client.name}")
         print("Produit  |Quantité    |Prix unitaire  |Prix total")
         print("-------------------------------------------------")
-        for item in client.basket:
+        for item in my_client.basket:
             # On calcule le prix et on déduit du stock
-            total_price=item.quantity*item.unit_price
+            total_price = item.quantity*item.unit_price
             print(f"{item.name}   |{item.quantity}  |{item.unit_price}  |total_price")
             amount += total_price
         print(f"Total : {amount}")
+        super.dailyBill += [self]
