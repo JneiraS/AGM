8eabc28f831d80765226d80cb20fafba187a6b5a
Yves LAMBERT <yves.lambert@fms-ea.com>
2024-07-24 11:52:06 +0200

    client

---
+++ src/client.py
@@ -0,0 +1,19 @@
+class Client:
+    """Ce qui permet à un client de faire ses achats"""
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.basket = []
+
+    def achat(self, product, quantity, unit_price):
+        """La quantité est exprimée en kilo (float ou à la pièce"""
+        self.basket += (product, quantity, unit_price)
+    def facture(self):
+        toll=0
+        for achat in self.basket:
+            (product, quantity, unit_price) = achat
+            toll += (quantity * unit_price)
+            # stock (product) -= quantity
+            # Ce n'est pas l'endroit pour le faire,
+            # mais ça doit être fait
+
