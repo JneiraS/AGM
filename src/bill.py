3567f0b73fa4c0aef9e45ca3515bdb6ab7c03e50
Yves LAMBERT <yves.lambert@fms-ea.com>
2024-07-25 14:01:37 +0200

    La note

--- src/bill.py
+++ src/bill.py
@@ -1,4 +1,4 @@
-import client
+from src.client import Client
 class Facturette: # Une ligne de bill
     name: str
     quantity: float
@@ -6,17 +6,18 @@ class Facturette: # Une ligne de bill
     total_price: float
 class Bill:
     dailyBill = []
-    def __init__(self, my_client: client.Client):
-        ligne: Facturette
+    def __init__(self, my_client: Client):
+        ligne: Facturette = Facturette()
         facture=[]
-        amount = 0.0
-        for item in my_client.basket:
+        self.amount = 0.0
+        print(len(my_client.basket.products))
+        for item in my_client.basket.products:
             # On calcule le prix
-            total_price = item.quantity * item.unit_price
+            total_price = item.stock * item.price_unit
             ligne.total_price = total_price
             ligne.name = item.name
-            ligne.quantity = item.quantity
-            ligne.unit_price = item.unit_price
+            ligne.quantity = item.stock
+            ligne.unit_price = item.price_unit
             self.amount += total_price
             facture += [ligne]
         Bill.dailyBill += [self]
@@ -24,7 +25,6 @@ class Bill:
     @classmethod
     def reset(cls):
         cls.dailyBill=[]
-        :
 
     def impression(self):
         print(f"Facture de {my_client.name} {my_client.surname}")
