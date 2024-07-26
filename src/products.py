4b1c58a24558aaff673a91fd7b3294a8d932e055
jneira <johnny.neira@gmail.com>
2024-07-26 19:29:50 +0200

    feat: Ajout de docstrings et ajustements des méthodes dans src/products.py
    - Docstrings ajoutées pour expliciter le rôle et l'utilisation des classes et méthodes.
    - Types de retour des méthodes factory_method affinés pour plus de précision.
    - Paramètres renommés pour mieux refléter leur utilisation prévue.

--- src/products.py
+++ src/products.py
@@ -7,6 +7,9 @@ from dataclasses import dataclass
 
 @dataclass
 class Product:
+    """
+    Représente un produit.
+    """
     name: str
     stock: float
     unit: str
@@ -18,28 +21,53 @@ class Product:
 
 @dataclass
 class Fruit(Product):
+    """
+    Représente un fruit.
+    Hérite de la classe Product et ajoute des fonctionnalités spécifiques aux fruits si nécessaire.
+    """
     ...
 
 
 @dataclass
 class Legume(Product):
+    """
+    Représente un légume.
+    Hérite de la classe Product et ajoute des fonctionnalités spécifiques aux fruits si nécessaire.
+    """
     ...
 
 
 class Creator(ABC):
-
+    """
+    Classe abstraite qui définit une interface pour créer des objets Product.
+    """
     @abstractmethod
-    def factory_method(self, line):
+    def factory_method(self, dataframe_line):
+        """
+        Méthode abstraite pour créer et retourner un objet Product à partir d'une ligne de données.
+        """
         pass
 
 
 class FruitCreator(Creator):
+    """
+    Implémente la méthode factory_method pour créer des objets Fruit.
+    """
 
-    def factory_method(self, line: list) -> Product:
-        return Fruit(line[1], line[2], line[3], line[4])
+    def factory_method(self, dataframe_line: list) -> Fruit:
+        """
+        Crée et retourne un nouvel objet Fruit à partir d'une ligne de données.
+        """
+        return Fruit(dataframe_line[1], dataframe_line[2], dataframe_line[3], dataframe_line[4])
 
 
 class LegumeCreator(Creator):
+    """
+    Implémente la méthode factory_method pour créer des objets Legume.
+    """
 
-    def factory_method(self, line: list) -> Product:
-        return Legume(line[1], line[2], line[3], line[4])
+    def factory_method(self, dataframe_line: list) -> Legume:
+        """
+        Crée et retourne un nouvel objet Legume à partir d'une ligne de données.
+        """
+        return Legume(dataframe_line[1], dataframe_line[2], dataframe_line[3], dataframe_line[4])
