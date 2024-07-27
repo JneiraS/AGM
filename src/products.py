3687f71abc9272cd5ac9a979869064a7a2f111f8
jneira <johnny.neira@gmail.com>
2024-07-27 21:29:29 +0200

    Amélioration de la gestion du stock dans la classe Product et mise à jour des créateurs de produits
    - Ajout d'un docstring détaillé pour la méthode `subtract_to_stock` dans la classe Product, clarifiant son fonctionnement et ses effets sur le stock d'un produit.
    - Modification de la signature de la méthode `subtract_to_stock` pour inclure une annotation de retour `-> None`, renforçant la sécurité de type.
    - Ajout de vérifications pour empêcher la réduction du stock en dessous de zéro, avec affichage d'un message d'avertissement approprié si nécessaire.
    - Mise à jour des classes dérivées Fruit et Legume pour inclure des docstrings et clarifier leur rôle dans l'héritage de la classe Product.
    - Refonte de la création d'objets dans les méthodes `factory_method` des classes FruitCreator et LegumeCreator pour une meilleure lisibilité et structure du code.
    
    Ces modifications visent à améliorer la robustesse, la lisibilité et la maintenabilité du code lié à la gestion des produits et de leur stock dans l'application.

--- src/products.py
+++ src/products.py
@@ -10,13 +10,25 @@ class Product:
     """
     Représente un produit.
     """
+
     name: str
     stock: float
     unit: str
     price_unit: float
 
-    def subtract_to_stock(self, amount: float):
-        self.stock -= amount
+    def subtract_to_stock(self, amount: float) -> None:
+        """
+        Soustrait une quantité spécifiée du stock actuel d'un produit.
+        Cette méthode vérifie d'abord si la soustraction de la quantité demandée entraînerait un stock
+        négatif pour le produit. Si c'est le cas, elle affiche un message d'avertissement et ne modifie pas
+        le stock. Sinon, elle met à jour le stock en soustrayant la quantité spécifiée.
+
+        :param amount: La quantité à soustraire du stock actuel du produit.
+        """
+        if self.stock - amount < 0:
+            print(f"Le stock de {self.name} ne peut être négatif ")
+        else:
+            self.stock -= amount
 
 
 @dataclass
@@ -25,6 +37,7 @@ class Fruit(Product):
     Représente un fruit.
     Hérite de la classe Product et ajoute des fonctionnalités spécifiques aux fruits si nécessaire.
     """
+
     ...
 
 
@@ -34,6 +47,7 @@ class Legume(Product):
     Représente un légume.
     Hérite de la classe Product et ajoute des fonctionnalités spécifiques aux fruits si nécessaire.
     """
+
     ...
 
 
@@ -41,6 +55,7 @@ class Creator(ABC):
     """
     Classe abstraite qui définit une interface pour créer des objets Product.
     """
+
     @abstractmethod
     def factory_method(self, dataframe_line):
         """
@@ -58,7 +73,9 @@ class FruitCreator(Creator):
         """
         Crée et retourne un nouvel objet Fruit à partir d'une ligne de données.
         """
-        return Fruit(dataframe_line[1], dataframe_line[2], dataframe_line[3], dataframe_line[4])
+        return Fruit(
+            dataframe_line[1], dataframe_line[2], dataframe_line[3], dataframe_line[4]
+        )
 
 
 class LegumeCreator(Creator):
@@ -70,4 +87,6 @@ class LegumeCreator(Creator):
         """
         Crée et retourne un nouvel objet Legume à partir d'une ligne de données.
         """
-        return Legume(dataframe_line[1], dataframe_line[2], dataframe_line[3], dataframe_line[4])
+        return Legume(
+            dataframe_line[1], dataframe_line[2], dataframe_line[3], dataframe_line[4]
+        )
